bb207dc55877d55693e85d4c3b1870de82843a6a
altai861 <altaizandanbileg@gmail.com>
2023-09-22 20:51:21 +0800

    Completed lab2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 		MyChain chain = null;
+		Object arr[] = null;
+		MyChain unitedChain = null;
+		MyChain intersectedChain = null;
 		Scanner scanner = new Scanner(System.in);
 		
 		while (true) {
@@ -45,12 +49,70 @@ public class Main {
 					System.out.println("Holboost jagsaalt uuseegui baina.");
 				}
 			} else if (songolt == 4) {
-				
+				if (chain != null) {
+					arr = chain.toArray();
+					System.out.println("Uussen array: "+ Arrays.toString(arr));
+				} else {
+					System.out.println("Holboost jagsaalt uuseegui baina.");
+				}
 			} else if (songolt == 5) {
+				
+				if (chain != null) {
+					System.out.println("Zalgah array iin heljeeg ogno uu");
+					int hemjee = scanner.nextInt();
+					Object[] newArray = new Object[hemjee];
+					for (int i = 0; i < hemjee; i++) {
+						System.out.println((i + 1) + " dahi elementiig oruulna uu: ");
+						int element = scanner.nextInt();
+						newArray[i] = element;
+					}
+					chain.addRange(newArray);
+					System.out.println("Shineer list zalgalaa");
+				} else {
+					System.out.println("Holboost jagsaalt uusegui baina.");
+				}
+				
 				
 			} else if (songolt == 6) {
 				
+				if (chain != null) {
+					MyChain newChain = new MyChain();
+					System.out.println("Nemj zalgah holboost jagsaaltiig uusgehiin tuld elementuudig oruulna uu. Bolson gej uzvel toonoos oor zuil oruulna uu");
+					
+					while (true) {
+						String element = scanner.next();
+						try {
+							int integerValue = Integer.parseInt(element);
+							newChain.add(newChain.size(), integerValue);
+						} catch (NumberFormatException e) {
+							break;
+						}
+					}
+					
+					unitedChain = chain.union(newChain);
+					System.out.println("Shine holboost jagsaaltiig zalgasan: " + unitedChain.toString());
+				} else {
+					System.out.println("Holboost jagsaalt uuseegui baina.");
+				}
+				
 			} else if (songolt == 7) {
+				if (chain != null) {
+					MyChain newChain = new MyChain();
+					System.out.println("Ogtloltsloliig oloh holboost jagsaaltiig uusgehiin tuld elementuudig oruulna uu. Bolson gej uzvel toonoos oor zuil oruulna uu");
+					while (true) {
+						String element = scanner.next();
+						try {
+							int integerValue = Integer.parseInt(element);
+							newChain.add(newChain.size(), integerValue);
+						} catch (NumberFormatException e) {
+							break;
+						}
+					}
+					intersectedChain = chain.intersection(newChain);
+					System.out.println("Shine holboost jagsaaltiin undsen jagsaalttai ogtloltsoh elementtin jagsaalt: " + intersectedChain.toString());
+				} else {
+					System.out.println("Holboost jagsaalt uuseegui baina.");
+				}
 				
 			} else if (songolt == 8) {
 				break;
